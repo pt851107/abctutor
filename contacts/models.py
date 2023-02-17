5c1ee8f0d67fb09e895713f70ccb85e32d0f661f
Wing kuk Cheng <>
2023-02-17 13:44:41 +0800

    comment 1st

---
+++ contacts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    title = models.CharField(max_length=100)
+    message = models.TextField(max_length=500)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
